cef8a7e64e7d48bed4e09d06c0ef23bd9a0d8754
Peter <nguyenvanninh_t63@hus.edu.vn>
2021-12-21 17:34:21 +0700

    ninh update code 21-12

--- app/controller/classroom_add_controller.php
+++ app/controller/classroom_add_controller.php
@@ -1,1 +1,36 @@
-//Ninh test branch
+<?php
+    require '../model/classroom.php';
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+         $id=$_POST['id'];
+         echo $id;
+     }
+    
+    $nameErr = $buildingErr = $descriptionErr = $avatarErr = "";
+    $name = $building = $description = $avatar = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Hãy nhập tên phòng học";
+        } else {
+            $name = ($_POST["name"]);
+        }
+                    
+        if($_POST['building'] == "none") {
+            $buildingErr = "Hãy chọn tòa nhà";
+        } else {
+            $building = ($_POST["building"]);
+        }
+
+        if (empty($_POST["description"])) {
+            $descriptionErr = "Hãy nhập mô tả";
+        } else {
+            $description = ($_POST["description"]);
+        }
+
+        if (empty($_POST["gender"])) {
+            $avatarErr = "Hãy chọn avatar";
+        } else {
+            $avatar = ($_POST["avatar"]);
+        }
+    }
+?>
---
+++ app/view/classroom_add_complete_view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../web/css/room/listRoom.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Thêm phòng học</title>
+</head>
+<body>
+    <?php
+        require '../controller/classroom_add_controller.php';
+        require '../common/define.php';
+    ?>
+    <div class="content container">
+        <div class='col-md-12' style="text-align: center;">
+            <p>Bạn đã đăng kí thành công phòng học.</p>
+            <a href="../view/classroom_add_input_view.php">Trở về trang chủ</p>
+        </div>
+    </div>
+    <script type="text/javascript" src=""></script>
+</body>
+</html>

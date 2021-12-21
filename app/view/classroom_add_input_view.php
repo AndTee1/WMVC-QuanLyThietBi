316146d87cfbd799895d53fc5a5ce9a841d63cb3
Peter <nguyenvanninh_t63@hus.edu.vn>
2021-12-21 17:41:41 +0700

    ninh update code 21-12

--- app/view/classroom_add_input_view.php
+++ app/view/classroom_add_input_view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../web/css/room/listRoom.css">
+    <link rel="stylesheet" href="../../web/css/room/addRoom.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <title>Thêm phòng học</title>
 </head>

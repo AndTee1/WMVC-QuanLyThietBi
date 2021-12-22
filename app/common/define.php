dd97aaa0a2a5f73417359b1d4046218199669ac0
AndTee1 <nguoithanhdat229@gmail.com>
2021-12-22 11:16:21 +0700

    phucph task UI teacher add

--- app/common/define.php
+++ app/common/define.php
@@ -1,4 +1,5 @@
 <?php 
+    // mảng tên tòa nhà 
     $listBuilding = [
         'k1'=> 'T1',
         'k2'=> 'T2',
@@ -6,4 +7,20 @@
         'k4'=> 'T4',
         'k5'=> 'T5',
         ];
+    // Mảng tên chuyên ngành
+    $listSpecialized = [
+        '001' => 'Khoa học máy tính',
+        '002' => 'Khoa học dữ liệu',
+        '003' => 'Hải dương học',
+
+    ];
+    // Mảng tên bằng cấp
+    $listDegree = [
+        '001'=> 'Cử nhân',
+        '002'=> 'Thạc sĩ',
+        '003'=> 'Tiến sĩ',
+        '004'=> 'Phó giáo sư',
+        '005'=> 'Giáo sư',
+
+    ];
 ?>
--- app/model/teacher.php
+++ app/model/teacher.php
@@ -1,3 +1,6 @@
 <?php
 	require '../common/connectDB.php';
+	$sqlTeacher = 'SELECT * FROM `teachers`';
+    $listTeacher = $conn ->query($sqlTeacher);
+    $listTeacher -> execute();
 ?>

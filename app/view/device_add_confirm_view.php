322f9259a02b1624c2c282fc64abe7d06e2bb9b5
quang tran <quangshinoda1511@gmail.com>
2021-12-23 23:18:50 +0700

    Add ui device adding feature

---
+++ app/view/device_add_confirm_view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Thêm thiết bị</title>
+        <link rel="stylesheet" href="../../web/css/device/addConfirmDevice.css">
+    </head>
+<body class="container">
+  <section class="section">
+    <p>Ten phong hoc</p>
+    <input placeholder="Nhap cmn di" />
+  </section>
+  <section class="section">
+    <p>Mo ta chi tiet</p>
+    <textarea class="txt"></textarea>
+  </section>
+  <section class="section">
+    <p>Avatar</p>
+    <label class="image" for="upload-photo">
+      <p>Upload vao day</p>
+    </label>
+    <input type="file" name="image" accept="image/*" class="upload" id="upload-photo">
+  </section>
+  <section>
+    <button class="btn btn-cancel">quay lai</button>
+    <button class="btn btn-submit">Xac nhan</button>
+  </section>
+</body>
+
+
+</html>

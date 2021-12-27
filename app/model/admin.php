e24d917419c2d4cf75656ae566e3a69129b25d4a
NguyetAnh-538 <tetmoon538@gmail.com>
2021-12-27 21:54:14 +0700

    add login

--- app/model/admin.php
+++ app/model/admin.php
@@ -1,11 +1,22 @@
 <?php
-	require '../common/connectDB.php';
+function checkAdmin($name, $pass)
+{
+    //require_once $_SERVER['DOCUMENT_ROOT'] . "/app/common/connectDB.php";
+    require 'app/common/connectDB.php';
+    $sql = "SELECT * FROM `admins` WHERE `login_id` = '{$name}' AND `password` = '{$pass}'";
+    $result = $conn->query($sql);
+    $result->execute();
+    if ($result->rowCount() > 0) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['name'] = $name;
 
-	function getLoginId($login_id){
-		$sql = $conn->prepare("SELECT `admins.login_id` FROM `admins` WHERE BINARY 
-			". $login_id . " = `admins.login_id`");
-        $sql ->execute();
-		return $sql;
-	}
-
-?>
+        date_default_timezone_set('Asia/Bangkok');
+        $_SESSION['time'] = date("Y-m-d H:i");
+        return true;
+    } else {
+        $_SESSION['loggedin'] = false;
+        $_SESSION['name'] = '';
+        $_SESSION['time'] = '';
+        return false;
+    }
+}

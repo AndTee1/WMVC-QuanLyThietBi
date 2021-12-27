7952dee8383052bd043d7f87b0e2732aede2d9d1
Đàm Thị Hồng Nhung <damthihongnhung2ktn@gmail.com>
2021-12-27 14:56:35 +0700

    update code

--- app/controller/admin_resetpass_controller.php
+++ app/controller/admin_resetpass_controller.php
@@ -1,1 +1,18 @@
-//hello
+<?php
+    require "../model/admin.php";
+
+    // require "../view/admin_request_view.php" ;
+
+    if(isset($_POST['login_id'])){
+        $login_id = $_POST['login_id'];
+        echo "hi";
+        $count = getLoginId($login_id) ->rowCount();
+
+        if($count == 1){
+            require "../view/admin_reset_view.php" ;
+        }else{
+            $error = "Login id không tồn tại trong hệ thống";
+        }
+    }
+
+?>
--- app/model/admin.php
+++ app/model/admin.php
@@ -1,4 +1,11 @@
 <?php
 	require '../common/connectDB.php';
 
+	function getLoginId($login_id){
+		$sql = $conn->prepare("SELECT `admins.login_id` FROM `admins` WHERE BINARY 
+			". $login_id . " = `admins.login_id`");
+        $sql ->execute();
+		return $sql;
+	}
+
 ?>

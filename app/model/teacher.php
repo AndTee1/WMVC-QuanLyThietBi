af838ce11cae440aaee19bdcdfd7473b4ed52543
AndTee1 <nguoithanhdat229@gmail.com>
2021-12-30 00:51:12 +0700

    BE add teacher

--- app/model/teacher.php
+++ app/model/teacher.php
@@ -1,7 +1,18 @@
 <?php
 	require '../common/connectDB.php';
+   
 	$sqlTeacher = 'SELECT * FROM `teachers`';
     $listTeacher = $conn ->query($sqlTeacher);
     $listTeacher -> execute();
+    function add( $name,$specialized,$degree,$avata,$description,$created){
+        require '../common/connectDB.php';
+        if($name!=""&& $specialized !=""&&$degree!=""&&$description!=""&&$avata!=""){
+            
+            $sql="INSERT INTO `teachers`(`name`, `avatar`, `description`, `specialized`, `degree`, `updated`, `created`) VALUES ('$name','$avata','$description','$specialized','$degree','','$created')";
+            $success = $conn -> query($sql);
+            $success->execute();
 
+        };
+    }
+        
 ?>

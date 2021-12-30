406fafb0b24bb58828d6ea905faa6107fe82552c
AndTee1 <nguoithanhdat229@gmail.com>
2021-12-30 08:56:39 +0700

    fix BE add teacher

--- app/model/teacher.php
+++ app/model/teacher.php
@@ -5,7 +5,7 @@
     $listTeacher = $conn ->query($sqlTeacher);
     $listTeacher -> execute();
     function add( $name,$specialized,$degree,$avata,$description,$created){
-        require '../common/connectDB.php';
+        global $conn;
         if($name!=""&& $specialized !=""&&$degree!=""&&$description!=""&&$avata!=""){
             
             $sql="INSERT INTO `teachers`(`name`, `avatar`, `description`, `specialized`, `degree`, `updated`, `created`) VALUES ('$name','$avata','$description','$specialized','$degree','','$created')";

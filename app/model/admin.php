36252b279101c48666c92bc082d6e922e0060236
Đàm Thị Hồng Nhung <damthihongnhung2ktn@gmail.com>
2021-12-30 09:47:24 +0700

    update function request

--- app/model/admin.php
+++ app/model/admin.php
@@ -1,11 +1,30 @@
 <?php
-	require '../common/connectDB.php';
 
+	// function return query get login_id
 	function getLoginId($login_id){
-		$sql = $conn->prepare("SELECT `admins.login_id` FROM `admins` WHERE BINARY 
-			". $login_id . " = `admins.login_id`");
+
+		// require file connect database
+		require '../common/connectDB.php';
+
+		// query
+		$sql = $conn->prepare("SELECT login_id FROM `admins` WHERE login_id = '" . $login_id . "' ");
         $sql ->execute();
+
 		return $sql;
+	}
+
+	// function update reset_password_token
+	function updateResetPasswordToken($login_id, $token){
+
+		// require file connect database
+		require '../common/connectDB.php';
+
+		// query
+		$sql = $conn->prepare("UPDATE `admins` SET `reset_password_token` = '" . $token . "' WHERE login_id = '" . $login_id . "' ");
+        $sql ->execute();
+
+		return $sql;
+
 	}
 
 ?>

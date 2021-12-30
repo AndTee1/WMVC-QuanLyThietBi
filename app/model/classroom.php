66a4500c30024e491980879101c697a9aa1a3512
Peter <nguyenvanninh_t63@hus.edu.vn>
2021-12-30 11:39:55 +0700

    ninh fix BE task add classroom

--- app/model/classroom.php
+++ app/model/classroom.php
@@ -5,17 +5,14 @@
     $listRoom = $conn ->query($sqlRoom);
     $listRoom -> execute();
 
-    // $name_r = $_POST['name_out'];
-    // $avatar_r = $_POST['avatar_out'];
-    // $description_r = $_POST['description_out'];
-    // $building_r = $_POST['building_out'];
-    // $updated_r = date("m/d/Y");
-    // $created_r = date("m/d/Y");
+    function add_room($name, $building, $description, $avatar, $updated, $created){
+        require '../common/connectDB.php';
+        if($name !="" && $building !="" && $description !="" && $avatar !=""){
 
-    // $sql = "INSERT INTO `classrooms` (`name`,`avatar`,`description`,`building`, `updated`, `created`) 
-    //         VALUES ('$name_r','$avatar_r','$description_r','$building_r','$updated_r','$created_r')";
-
-    // $conn -> exec($sql);
-    // $conn = null;
-    // header('Location: ../view/classroom_add_complete_view.php');
+            $sql = "INSERT INTO `classrooms` (`name`,`avatar`,`description`,`building`, `updated`, `created`) 
+                     VALUES ('$name','$avatar','$description','$building','$updated','$created')";
+            $succ = $conn -> query($sql);
+            $succ->execute();
+        };
+    }
 ?>

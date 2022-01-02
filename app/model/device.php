0c39990ef53258137823a10b41ae240b3f197e04
Ngoc Son <phamngocson2k2@gmail.com>
2022-01-02 22:27:22 +0700

    Update models/device.php

--- app/model/device.php
+++ app/model/device.php
@@ -12,5 +12,26 @@
 	$listDevices = $conn ->prepare($sqlDevices);
 	$listDevices -> execute();
 
+	function searchGiveBackBook($equipment, $teacher, $classroom){  
+        require '../common/connectDB.php';
+        $sqlDevicesGiveBackBook = "SELECT device_transactions.id, devices.name AS deviceName, teachers.name AS teacherName, classrooms.name AS classroomName,device_transactions.returned_date 
+                                    FROM device_transactions 
+                                    CROSS JOIN devices ON device_transactions.device_id = devices.id 
+                                    CROSS JOIN teachers ON device_transactions.teacher_id = teachers.id 
+                                    CROSS JOIN classrooms ON device_transactions.classroom_id = classrooms.id
+                                    WHERE devices.name = '$equipment' AND teachers.name = '$teacher' AND classrooms.name = '$classroom'";
+        $listDeviceGiveBack = $conn ->prepare($sqlDevicesGiveBackBook);     
+        $listDeviceGiveBack->execute(); 
+        $result = $listDeviceGiveBack->fetchAll(PDO::FETCH_OBJ);
+        
+        return $result;
+    }                       
+      
+    function givebackbook($id, $time){
+        require '../common/connectDB.php';
+        $sql = "UPDATE `device_transactions` SET `returned_date`='$time' WHERE id = '$id'";
+        $deviceName = $conn -> prepare($sql);
+        $deviceName -> execute();
+    }
 ?>
 

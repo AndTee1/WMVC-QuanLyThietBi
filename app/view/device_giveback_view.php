cfe24231846a4578f99417d3a47cc02b04b506f0
Ngoc Son <phamngocson2k2@gmail.com>
2022-01-05 21:46:40 +0700

    Update css in corner

--- app/view/device_giveback_view.php
+++ app/view/device_giveback_view.php
@@ -13,10 +13,13 @@
         date_default_timezone_set("Asia/Ho_Chi_Minh");
 ?>
 
+
 <button class="custombackhome"><a href="../../home.php"><img src="https://img.icons8.com/material-outlined/24/FFFFFF/home--v2.png"/>  Trang chủ  </a></button>
 
 <form method="post">
-    
+       
+        
+
         <!-- Thiết bị -->
         <div>
             <label>Thiết bị</label>

7779815d273c3747a63af6baf107f0d41e5307d9
Ngoc Son <phamngocson2k2@gmail.com>
2022-01-05 09:33:52 +0700

    Update giveback controller

--- app/controller/device_giveback_controller.php
+++ app/controller/device_giveback_controller.php
@@ -4,7 +4,6 @@
     require '../model/classroom.php';
     
     $equipment = $teacher = $classroom = "";
-    $equipmentErr = $teacherErr = $classroomErr = "";
     $idgive=$idgive2=$timegive="";
     $count = 0;
     $i = 1;
@@ -12,42 +11,10 @@
 
     if (isset($_POST["search"])) {
         // error_reporting(0);
-
-        if (empty($_POST["equipment"])) {
-            $equipmentErr = "Hãy nhập tên thiết bị";
-        } 
-        else {
-            if($listDevices->rowCount()>0) 
-            {
-                $result=$listDevices->fetchAll(PDO::FETCH_OBJ);
-                for ($i = 0; $i <  count($result); $i++)  
-                {
-                    if($_POST["equipment"] != $result[$i]->name){
-                        $count;
-                    } else {
-                        $count++;
-                    }
-                }
-                if($count == 1){
-                    $equipment = ($_POST["equipment"]);
-                } else {
-                    $equipmentErr = "Tên thiết bị sai *";
-                }
-                
-            }
-        }
-                    
-        if($_POST['teacher'] == "") {
-            $teacherErr = "Hãy chọn giáo viên *";
-        } else {
-            $teacher = ($_POST["teacher"]);
-        }
-
-        if($_POST['classroom'] == "") {
-            $classroomErr = "Hãy chọn lớp học *";
-        } else {
-            $classroom = ($_POST["classroom"]);
-        }   
+        $equipment = ($_POST["equipment"]);
+        $teacher = ($_POST["teacher"]);
+        $classroom = ($_POST["classroom"]);
+          
     }
 
    

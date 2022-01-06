512e716a1d4673f0d1ae2e1eddf285fb0cd74df9
Nghiem Thuy Quynh <67702204+quynh00@users.noreply.github.com>
2022-01-06 23:22:58 +0700

    update device search advance

--- app/view/device_seach_advaned_view.php
+++ app/view/device_seach_advaned_view.php
@@ -5,24 +5,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TÌm kiếm nâng cao</title>
+    <title>Tìm kiếm nâng cao</title>
     <link rel="stylesheet" href="../../web/css/device/searchAdvancedDevice.css">
 </head>
 
 <body>
+    <button class="custombackhome"><a href="../../home.php"><img src="https://img.icons8.com/material-outlined/24/FFFFFF/home--v2.png"/>Trang chủ</a></button>
     <div class="content container">
         <form action="" method="GET">
             <div class="search">
                 <div class="search_keyword">
                     <label >Từ khóa</label>
-                    <input type="text" name="keyword">
+                    <input type="text" value="<?php if(isset($_GET['keyword'])) echo $_GET['keyword'] ?>" name="keyword">
                 </div>
                 <div class="search_status">
-                    <label >Tình trạng</label>
-                    <select id="status"  name="status">
+                    <label>Trạng thái</label>
+                    <select id="status" name="status">
                         <option value=''></option>
-                        <option>Đang rảnh</option>
-                        <option>Đang mượn</option>
+                        <option value="1" <?php if(isset($_GET['status']) && $_GET['status'] == 1) echo 'selected' ?> >Đang rảnh</option>
+                        <option value="2"  <?php if(isset($_GET['status']) && $_GET['status'] == 2) echo 'selected' ?>>Đang mượn</option>
                     </select>
                 </div>          
                 <div>
@@ -30,8 +31,9 @@
                 </div>
             </div>
         </form>
+            <?php require '../controller/device_search_advaned_controller.php'; ?>
         <div class="count_device">
-            <p>Số thiết bị tìm thấy:</p>
+            <p>Số thiết bị tìm thấy: <?php echo count($result) ?></p>
         </div>
 
         <table>
@@ -41,29 +43,26 @@
                 <th id="th_status">Trạng thái</th>
                 <th id="th_action">Action</th>
             </tr>
-            <?php require '../controller/device_search_advaned_controller.php';?>
-            <?php
-            foreach ($r as $row) { ?>
+            <?php 
+           
+           for ($i = 0; $i <  count($result); $i++){  ?>
                 <tr>
-                    <td><?php echo $row['id'] ?></td>
-                    <td><?php echo $row['name'] ?></td>
+                    <td><?php echo $i+1 ?></td>
+                    <td><?php echo $result[$i]['name'] ?></td>
                     <td>
-                        <?php                             
-                            if($row['returned_date'] === null and $row['device_id'] !== null){
-                                echo "Đang mượn";
-                            }
-                            else{
-                                echo "Đang rảnh";
-                            }
+                        <?php
+                        if (isset($result[$i]['status']) && $result[$i]['status'] == 2 ) {
+                            echo "Đang mượn";
+                        } else {
+                            echo "Đang rảnh";
+                        }
                         ?>
                     </td>
                     <td>
-                        <?php  
-                            if($row['returned_date'] === null and $row['device_id'] !== null){            
-                            }else{                           
-                                $b='<button id="btn_borrow"><a href="device_borrow_view.php">Mượn</a></button>';
-                                echo $b;
-                            }
+                        <?php
+                        if (isset($result[$i]['status']) && $result[$i]['status'] == 1) {
+                            echo $action='<button id="btn_borrow"><a href="device_borrow_view.php">Mượn</a></button>';
+                        } 
                         ?>
                     </td>
                 </tr>

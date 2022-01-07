a3ba57e7bf39b8b315317945f19b4dec34cd9b33
MQUANG <nmquang21@gmail.com>
2022-01-07 22:42:51 +0700

    them nut sua thiet bi

--- app/view/device_search_view.php
+++ app/view/device_search_view.php
@@ -63,7 +63,7 @@
                             
                            //$b = '<button class="btn_delete"><a href="device_delete_view.php?id=' .  $row['id'] . '">Xóa</a></button> <button class="btn_delete"><a href="device_edit_view.php?id=' .  $row['id'] . '">Sửa</a></button>';
                            
-                          $b = '<a href="../controller/device_search_controller.php?delete=true&id=' . $row['id'] .'" class="btn_delete" onclick="return confirm(`Bạn chắc chắn muốn xóa thiết bị?`)">Xóa</a>';
+                          $b = '<a href="../controller/device_search_controller.php?delete=true&id=' . $row['id'] .'" class="btn_delete" onclick="return confirm(`Bạn chắc chắn muốn xóa thiết bị?`)">Xóa</a><a class="btn_edit" href="device_edit_view.php?id=' .  $row['id'] . '">Sửa</a>';
                             echo $b;
                         } else {
                             

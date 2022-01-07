71cac707dd19ee2572f741fe9a14528340760cce
Quan99 <trannhuquan_t62@hus.edu.vn>
2022-01-07 13:28:08 +0700

    up device edit

--- app/view/device_edit_input_view.php
+++ app/view/device_edit_input_view.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../web/css/room/editRoom.css">
+    <link rel="stylesheet" href="../../web/css/device/editDevice.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <title>Sửa thiết bị</title>
 </head>

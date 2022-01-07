bd0974a980683d6499fe53d4569d87a2c47439a3
NguyetAnh-538 <tetmoon538@gmail.com>
2022-01-07 22:18:17 +0700

    refactor

--- web/js/addDevice.js
+++
@@ -1,3 +0,0 @@
-function addAvatarDevice(target) {
-    document.getElementById("addAvatar").value = target.files[0].name;
-}
--- web/js/addRoom.js
+++
@@ -1,8 +0,0 @@
-if (window.history.replaceState) {
-    window.history.replaceState(null, null, window.location.href);
-}
-
-function myFunction(target) {
-    document.getElementById("avatar").value = target.files[0].name;
-}
-
